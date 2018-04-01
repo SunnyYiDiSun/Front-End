a8e52e1474079e6d2929e05c5fb8437ac52d344e
SunnyYiDiSun <455096252@qq.com>
2018-04-01 21:46:00 +0800

    some changes

--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -1,5 +1,6 @@
-import type from '@/store/type.js'
-import axios from 'axios'
+import type from '@/store/type.js';
+import axios from 'axios';
+import requestUrl from '@/config/requestUrl.js';
 const state={
 	name:'',
 	loginStatus:false,
@@ -13,26 +14,24 @@ const getters={
 }
 //异步操作
 const actions={
-	login({commit,dispacth},{name,passWord,loginStatus}){
-    axios.post("http://localhost:8090/name/1",{
+	login({commit},{name,passWord,loginStatus}){
+    axios.post(requestUrl.login.url,{
        	'name':name,
        	'passWord':passWord,
         'loginStatus':loginStatus
        }).then(({data}) =>{
           commit(type.login,data);
-       },(response) =>{
-          console.info('failed response=========>'+response);
+       }).catch(function (error) {
+          console.log(error);
        });
-       
+
 	}
 }
 //数据的改变
 const mutations={
    [type.login](state,{loginStatus}){
-      console.info(loginStatus);
       state.loginStatus=loginStatus;
       state.loginTrigger=!state.loginTrigger;
-      console.info(state);
    }
 }
 export default {
